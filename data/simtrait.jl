3cc5266ec6d92974f9c919e30f932921d4a8745c
Julien St-Pierre <justp@DESKTOP-81A9I6M.localdomain>
2022-01-18 16:57:28 -0500

    Sample subjects for training and test sets.

--- data/simtrait.jl
+++ data/simtrait.jl
@@ -7,7 +7,7 @@ using CSV, DataFrames, SnpArrays, DataFramesMeta, StatsBase, LinearAlgebra, Dist
 # Initialize parameters
 # ------------------------------------------------------------------------
 # Assign default command-line arguments
-const ARGS_ = isempty(ARGS) ? ["0.8", "0", "0.1", "10000", "0.005", "ALL", "data/"] : ARGS
+const ARGS_ = isempty(ARGS) ? ["0.8", "0", "0.1", "10000", "0.005", "ALL", ""] : ARGS
 
 # Fraction of variance due to polygenic additive effect (logit scale)
 h2_g = parse(Float64, ARGS_[1])
@@ -43,19 +43,27 @@ dat = @chain CSV.read("1000G/covars.csv", DataFrame) begin
     @select!(:FID, :IID, :POP, :SEX, :AGE, :PC1, :PC2, :PC3, :PC4, :PC5, :PC6, :PC7, :PC8, :PC9, :PC10)
 end	    	  
 
+# Randomly sample subjects by POP for training and test sets
+grpdat = groupby(dat, :POP)
+train_ids = [sample(grpdat[i].IID, Int(ceil(nrow(grpdat[i]) * 0.80)); replace = false) for i in 1:length(grpdat)] |>
+                x -> reduce(vcat, x)
+
+# Add indicator variable for training subjects
+dat.train = [dat.IID[i] in train_ids for i in 1:n]
+
 #-------------------------------------------------------------------------
 # Load genotype Data
 #-------------------------------------------------------------------------
 # Read plink bim file
 _1000G = SnpArray("1000G/1000G.bed")
 
-# Compute MAF for each SNP and in each Population
+# Compute MAF in training set for each SNP and in each Population
 _maf = DataFrame()
-_maf.EUR = maf(@view(_1000G[dat.POP .== "EUR", :]))
-_maf.EAS = maf(@view(_1000G[dat.POP .== "EAS", :]))
-_maf.AMR = maf(@view(_1000G[dat.POP .== "AMR", :]))
-_maf.SAS = maf(@view(_1000G[dat.POP .== "SAS", :]))
-_maf.AFR = maf(@view(_1000G[dat.POP .== "AFR", :]))
+_maf.EUR = maf(@view(_1000G[(dat.POP .== "EUR") .& dat.train, :]))
+_maf.EAS = maf(@view(_1000G[(dat.POP .== "EAS") .& dat.train, :]))
+_maf.AMR = maf(@view(_1000G[(dat.POP .== "AMR") .& dat.train, :]))
+_maf.SAS = maf(@view(_1000G[(dat.POP .== "SAS") .& dat.train, :]))
+_maf.AFR = maf(@view(_1000G[(dat.POP .== "AFR") .& dat.train, :]))
 
 # Compute range for MAFs among the 5 populations
 _maf.range = vec(maximum([_maf.EUR _maf.EAS _maf.AMR _maf.SAS _maf.AFR], dims = 2) - minimum([_maf.EUR _maf.EAS _maf.AMR _maf.SAS _maf.AFR], dims = 2))
@@ -112,7 +120,7 @@ beta = rand.([Normal(0, sqrt(W[i])) for i in 1:p])
 
 # Simulate fixed effect for dichotomous environmental effect
 Z = (dat.POP .== ["SAS" "AFR"]) |>
-    x -> x ./ std(x, dims=1)
+    x -> x ./ std(x, dims=1, corrected = false)
 gamma = rand(Normal(0, sqrt(sigma2_d/2)), 2)
 
 # Simulate random effects
@@ -129,7 +137,7 @@ expit(x) = exp(x) / (1 + exp(x))
 final_dat = @chain dat begin
 	@transform!(:logit_pi = logit(pi0) .- log(1.3) * :SEX + log(1.05) * (:AGE / 10) + G * beta + Z * gamma + b)
     @transform!(:pi = expit.(:logit_pi))
-    @transform(:y = rand.([Binomial(1, :pi[i]) for i in 1:n]))
+    @transform(:y = rand.([Binomial(1, :pi[i]) for i in 1:nrow(dat)]))
     select!(Not([:pi, :logit_pi]))
 end
 
