cc4924c0a96205924a311894bc94f95a5dddd040
Julien St-Pierre <justp@DESKTOP-81A9I6M.localdomain>
2022-01-18 17:18:29 -0500

    Added train and test sets.

--- simulations/ggmix.R
+++ simulations/ggmix.R
@@ -15,10 +15,11 @@ library(data.table)
 #=======================================================================
 #Read phenotype and covariates file
 pheno.cov <- read.table("covariate.txt", sep=",", header = T) %>%
-			 mutate(ID=paste(FID,":",IID,sep=""))
-			 
+  mutate(ID=paste(FID,":",IID,sep=""))
+
 n <- nrow(pheno.cov)	
-		 
+trainrowinds = which(pheno.cov$train == "true")
+
 #=======================================================================
 # Load the genotype data
 #=======================================================================
@@ -29,8 +30,11 @@ snp_readBed("geno.bed",backingfile = tmpfile)
 obj.bigSNP <- snp_attach(paste0(tmpfile, ".rds"))
 p <- nrow(obj.bigSNP$map)
 G <- bigsnpr::snp_fastImputeSimple(obj.bigSNP$genotypes)[,1:p]
+
+# Standardize genotype matrix
 s <- apply(G, 2, sd)
 G <- scale(G)
+Gtrain <- G[trainrowinds,]
 
 #Read GRM matrix
 GRM <- as.matrix(fread("grm.txt.gz"))
@@ -40,13 +44,14 @@ rownames(GRM) <- pheno.cov$ID
 #=======================================================================
 # GGMIX
 #=======================================================================
-X <- pheno.cov[,c("AGE","SEX")]
+Xtrain <- pheno.cov[trainrowinds, c("AGE","SEX")]
+ytrain <- as.matrix(pheno.cov$y)[trainrowinds]
 
-fit_ggmix <- ggmix(x = as.matrix(cbind(X, G)),
-                   y = as.matrix(pheno.cov$y),
+fit_ggmix <- ggmix(x = as.matrix(cbind(Xtrain, Gtrain)),
+                   y = ytrain,
                    standardize = TRUE,		
-                   kinship=GRM,
-                   penalty.factor = c(rep(0,ncol(X)),rep(1,p))
+                   kinship=GRM[trainrowinds, trainrowinds],
+                   penalty.factor = c(rep(0,ncol(Xtrain)),rep(1,p))
 )
 
 # Find lambda that gives minimum GIC				  
@@ -59,12 +64,16 @@ ggmixBIC_beta <- 1/s * coef(bic)[setdiff(rownames(coef(bic)), c("(Intercept)","A
 
 # Read file with real values
 true_betas = read.csv("betas.txt")$beta
-ggmix_betas = 1/s * fit_ggmix$beta[-(1:ncol(X)),]
+ggmix_betas = 1/s * fit_ggmix$beta[-(1:ncol(Xtrain)),]
 
 # False positive rate (FPR) at 1%
 v <- apply((ggmix_betas != 0) & (true_betas == 0), 2, sum)/sum(true_betas == 0) < fpr
 ggmixFPR_beta <- ggmix_betas[,tapply(seq_along(v), v, max)["TRUE"]]
-ggmixFPR_yhat <- predict(fit_ggmix, as.matrix(cbind(X, G)))[,tapply(seq_along(v), v, max)["TRUE"]]
+
+# Predict phenotype on test set
+Xtest <- pheno.cov[-trainrowinds, c("AGE","SEX")]
+Gtest <- G[-trainrowinds,]
+ggmixFPR_yhat <- predict(fit_ggmix, as.matrix(cbind(Xtest, Gtest)), covariance = GRM[-trainrowinds, trainrowinds])[,tapply(seq_along(v), v, max)["TRUE"]]
 
 #Save results
 write.csv(cbind(ggmixAIC = ggmixAIC_beta, ggmixBIC = ggmixBIC_beta, ggmixFPR = ggmixFPR_beta), "ggmix_results.txt", quote=FALSE, row.names = FALSE)
