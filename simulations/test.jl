7d813b499f3c22080bddc4fbe18bdab67587e659
Julien St-Pierre <justp@DESKTOP-81A9I6M.localdomain>
2022-01-18 16:56:44 -0500

    Filter for training and test subjects. Added predict function.

--- simulations/test.jl
+++ simulations/test.jl
@@ -15,39 +15,42 @@ const grmfile = datadir * "grm.txt.gz"
 #-------------------------------------------------------------------
 # PenalizedGLMM
 #-------------------------------------------------------------------
+# Read covariate file
+covdf = CSV.read(covfile, DataFrame)
+trainrowinds = findall(covdf.train)
+testrowinds = setdiff(1:nrow(covdf), trainrowinds)
+
 # Fit null model with one random effect
-nullmodel = pglmm_null(@formula(y ~ SEX + AGE), covfile, grmfile)
+nullmodel = pglmm_null(@formula(y ~ SEX + AGE), covfile, grmfile, covrowinds = trainrowinds, grminds = trainrowinds)
 
 # Fit a penalized logistic mixed model
-modelfit = pglmm(nullmodel, plinkfile, verbose = true, GIC_crit = "ALL")
+modelfit = pglmm(nullmodel, plinkfile, geneticrowinds = trainrowinds, verbose = true)
 
 # Genetic predictors effects at each λ   
 pglmm_β = modelfit.betas[3:end,:]
 
 # Find λ that gives minimum GIC
-pglmmAIC_β = pglmm_β[:, argmin(modelfit.GIC["AIC",:])]
-pglmmBIC_β = pglmm_β[:, argmin(modelfit.GIC["BIC",:])]
+pglmmAIC_β = PenalizedGLMM.GIC(modelfit, :AIC)[3:end]
+pglmmBIC_β = PenalizedGLMM.GIC(modelfit, :BIC)[3:end]
 
 #----------------------------
 # GLMNet
 #----------------------------
-# convert PLINK genotype to matrix, convert to additive model (default), scale and impute
+# convert PLINK genotype to matrix, convert to additive model (default) and impute
 geno = SnpArray(plinkfile * ".bed")
-G = convert(Matrix{Float64}, geno, model = ADDITIVE_MODEL, impute = true)
+G = convert(Matrix{Float64}, @view(geno[trainrowinds,:]), model = ADDITIVE_MODEL, impute = true)
 p = size(G, 2)
 
-# Read covariate file
-covdf = CSV.read(covfile, DataFrame)
-
 # y must be a matrix with one column per class
-y = convert(Matrix{Float64}, [covdf.y .== 0 covdf.y .== 1])
+y = convert(Matrix{Float64}, [covdf.y .== 0 covdf.y .== 1]) |> 
+        x -> x[trainrowinds,:]
 
 #----------------------------
 # Lasso with no PC
 #----------------------------
 # Combine covariate with genetic predictors
 varlist = ["AGE", "SEX"]
-X = [Array(covdf[:, varlist]) G]
+X = [Array(covdf[trainrowinds, varlist]) G]
 
 # Fit a penalized logistic model using GLMNet with no PCs
 fit_glmnet = glmnet(X, y, Binomial(), penalty_factor = [zeros(length(varlist)); ones(p)])
@@ -62,7 +65,7 @@ cv_glmnet_β = cv_glmnet.path.betas[(length(varlist) + 1):end, argmin(cv_glmnet.
 #----------------------------
 # Combine covariate with genetic predictors
 varlistwithPC = ["AGE", "SEX", "PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10"]
-XwithPC = [Array(covdf[:, varlistwithPC]) G]
+XwithPC = [Array(covdf[trainrowinds, varlistwithPC]) G]
 
 # Fit a penalized logistic model using GLMNet with 10 PCs
 fit_glmnetPC = glmnet(XwithPC, y, Binomial(), penalty_factor = [zeros(length(varlistwithPC)); ones(p)])
@@ -91,20 +94,25 @@ betas.cv_glmnetPC = cv_glmnetPC_β
 yhat = DataFrame()
 fpr = parse(Float64, ARGS_[2])
 
+# Create Arrays for test set
+Gnew = convert(Matrix{Float64}, @view(geno[testrowinds,:]), model = ADDITIVE_MODEL, impute = true)
+Xnew = [Array(covdf[testrowinds, varlist]) Gnew]
+XwithPCnew = [Array(covdf[testrowinds, varlistwithPC]) Gnew]
+
 # pglmm
 pglmmFPR_ind = findlast(sum((pglmm_β .!= 0) .& (betas.beta .== 0), dims = 1) / sum(betas.beta .== 0) .< fpr)[2]
 betas.pglmmFPR = pglmm_β[:, pglmmFPR_ind]
-yhat.pglmmFPR = PenalizedGLMM.predict(modelfit, X, outtype = :prob)[:,pglmmFPR_ind]
+yhat.pglmmFPR = PenalizedGLMM.predict(modelfit, Xnew, grmfile, grmrowinds = testrowinds, grmcolinds = trainrowinds, s = [pglmmFPR_ind], outtype = :prob)
 
 # glmnet with no PCs
 glmnetFPR_ind = findlast(sum((glmnet_β .!= 0) .& (betas.beta .== 0), dims = 1) / sum(betas.beta .== 0) .< fpr)[2]
 betas.glmnetFPR = glmnet_β[:, glmnetFPR_ind]
-yhat.glmnetFPR = GLMNet.predict(fit_glmnet, X, outtype = :prob)[:,glmnetFPR_ind]
+yhat.glmnetFPR = GLMNet.predict(fit_glmnet, Xnew, outtype = :prob)[:,glmnetFPR_ind]
 
 # glmnet with 10 PCs
 glmnetPCFPR_ind = findlast(sum((glmnetPC_β .!= 0) .& (betas.beta .== 0), dims = 1) / sum(betas.beta .== 0) .< fpr)[2]
 betas.glmnetPCFPR = glmnetPC_β[:, glmnetPCFPR_ind]
-yhat.glmnetPCFPR = GLMNet.predict(fit_glmnetPC, XwithPC, outtype = :prob)[:,glmnetPCFPR_ind]
+yhat.glmnetPCFPR = GLMNet.predict(fit_glmnetPC, XwithPCnew, outtype = :prob)[:,glmnetPCFPR_ind]
 
 #-----------------------
 # Save results
