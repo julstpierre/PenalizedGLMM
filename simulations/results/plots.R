2904655f659b080a0564c8cc48b4e05cf2f07c72
Julien St-Pierre <julienst-pierre@MacBook-Air-de-Julien.local>
2022-09-27 08:44:59 -0400

    Minor updates.

--- simulations/results/plots.R
+++ simulations/results/plots.R
@@ -79,7 +79,7 @@ size2plot <- function(file, ylab, geo_ = NULL, K_ = NULL, methods = NULL, kin_ =
     ggplot(df,aes(x=size,y=mean, linetype = method, color = method))+
       geom_line(size = 0.75)+
       labs(x="Model size",y=ylab)+
-      scale_color_brewer(palette = "Set1") +
+      scale_color_brewer(palette = "Spectral") +
       labs(color  = "method", linetype = "method")+
       theme_bw() + 
       theme(plot.background = element_blank(),
@@ -117,16 +117,16 @@ size2plot(file="size_tpr.csv", ylab="TPR", geo = c("1d", "ind"), K = c(10, 20))
 kin = "ALL"
 
 # AUC
-p1 <- size2plot(file="size_auc.csv", ylab="AUC", geo = c("UKBB"), methods = c("pglmm","glmnetPC"), kin_ = kin)
+p1 <- size2plot(file="size_auc.csv", ylab="AUC", geo = c("UKBB"), kin_ = kin)
 
 # Bias
 #size2plot(file="size_bias.csv", ylab="Relative bias (%)", geo = c("UKBB"))
 
 # RMSE
-p2 <- size2plot(file="size_rmse.csv", ylab=expression(paste("RMSE (", hat(beta), ")")), geo = c("UKBB"), methods = c("pglmm", "glmnetPC"), kin_ = kin)
+p2 <- size2plot(file="size_rmse.csv", ylab=expression(paste("RMSE (", hat(beta), ")")), geo = c("UKBB"), kin_ = kin)
 
 # TPR
-p3 <- size2plot(file="size_tpr.csv", ylab="TPR", geo = c("UKBB"), methods = c("pglmm", "glmnetPC"), kin_ = kin)
+p3 <- size2plot(file="size_tpr.csv", ylab="TPR", geo = c("UKBB"), kin_ = kin)
 
 ggarrange(p1, p2, p3, common.legend = TRUE, legend = "bottom", ncol = 2, nrow = 2)
 
@@ -135,7 +135,7 @@ ggarrange(p1, p2, p3, common.legend = TRUE, legend = "bottom", ncol = 2, nrow = 
 #----------------------------
 
 # Function that reads file with results and create a plot using ggplot2
-df2boxplot <- function(file, ylab, methods, geo_ = NULL, K_ = NULL, kin_ = "NONE", logscale = TRUE, change_labels = TRUE, print_table = FALSE){
+df2boxplot <- function(file, ylab, methods, geo_ = NULL, K_ = NULL, kin_ = "NONE", logscale = TRUE, ymin = NA, ymax = NA, change_labels = TRUE, print_table = FALSE){
   # Read files
   if (!is.null(K_)){
     df <- read.csv2(file, sep=",") %>% 
@@ -164,11 +164,12 @@ df2boxplot <- function(file, ylab, methods, geo_ = NULL, K_ = NULL, kin_ = "NONE
   
   if (change_labels == TRUE){
     df[df$method == "ggmixBIC", "method"] <- "ggmix (BIC)"
-    df[df$method == "ggmix", "method"] <- "ggmix (CV)"
+    #df[df$method == "ggmix", "method"] <- "ggmix (CV)"
     df[df$method == "cv_glmnetPC", "method"] <- "glmnetPC (10-fold CV)"
-    df[df$method == "glmnetPC", "method"] <- "glmnetPC (CV)"
+    #df[df$method == "glmnetPC", "method"] <- "glmnetPC (CV)"
+    #df[df$method == "glmnet", "method"] <- "glmnet (CV)"
     df[df$method == "pglmmAIC", "method"] <- "pglmm (AIC)"
-    df[df$method == "pglmmBIC", "method"] <- "pglmm (BIC)"
+    df[df$method == "pglmmBIC", "method"] <- "standard gBLUP"
     df[df$method == "pglmm", "method"] <- "pglmm (CV)"
   }
   
@@ -178,10 +179,10 @@ df2boxplot <- function(file, ylab, methods, geo_ = NULL, K_ = NULL, kin_ = "NONE
       summarise(mean = mean(value), 
                 sd = sd(value),
                 median = median(value), 
-                IRQ = IQR(value))
+                IQR = IQR(value))
   } else if (length(unique(df$geo)) > 1){
     ggplot(df,aes(x=method,y=value,color = method))+
-      geom_boxplot()+
+      geom_boxplot(outlier.shape=)+
       facet_grid(K~geo, labeller=label_bquote(cols=.(geo),rows=K==.(K)))+
       labs(x="Method",y=ylab)+
       scale_color_brewer(palette = "Set1") +
@@ -196,129 +197,99 @@ df2boxplot <- function(file, ylab, methods, geo_ = NULL, K_ = NULL, kin_ = "NONE
       theme(axis.line.x = element_line(color="black", size = 0.5),
             axis.line.y = element_line(color="black", size = 0.5))
   } else{
-    ggplot(df,aes(x=method,y=value,color = method))+
-      geom_boxplot()+
-      labs(x="Method",y=ylab)+
-      scale_color_brewer(palette = "Set1") +
-      theme_bw() + 
-      theme(axis.title.x=element_blank(),
-            axis.text.x=element_blank(),
-            axis.ticks.x=element_blank())+
-      theme(plot.background = element_blank(),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank() )+
-      #theme(panel.border= element_blank())+
-      theme(axis.line.x = element_line(color="black", size = 0.5),
-            axis.line.y = element_line(color="black", size = 0.5))
+      if (!all(is.na(c(ymax,ymin)))){
+      ggplot(df,aes(x=method,y=value,color = method))+
+        geom_boxplot()+
+        ylim(ymin,ymax)+
+        labs(x="Method",y=ylab)+
+        scale_color_brewer(palette = "Set2") +
+        theme_bw() + 
+        theme(axis.title.x=element_blank(),
+              axis.text.x=element_blank(),
+              axis.ticks.x=element_blank())+
+        theme(plot.background = element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank() )+
+        #theme(panel.border= element_blank())+
+        theme(axis.line.x = element_line(color="black", size = 0.5),
+              axis.line.y = element_line(color="black", size = 0.5))
+      } else {
+        ggplot(df,aes(x=method,y=value,color = method))+
+          geom_boxplot()+
+          labs(x="Method",y=ylab)+
+          scale_color_brewer(palette = "Set2") +
+          theme_bw() + 
+          theme(axis.title.x=element_blank(),
+                axis.text.x=element_blank(),
+                axis.ticks.x=element_blank())+
+          theme(plot.background = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank() )+
+          #theme(panel.border= element_blank())+
+          theme(axis.line.x = element_line(color="black", size = 0.5),
+                axis.line.y = element_line(color="black", size = 0.5))
+      }
   }
   
 }
-
-#---------------------
-# Simulated genotypes
-#---------------------
-
-#------------------
-## AIC vs BIC vs CV
-#------------------
-# Model size
-p1 <- df2boxplot(file="model_size.csv", ylab=expression(paste("Model size (log"[10], " scale)", sep="")), methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("ind"), K = c(20))
-
-# AUC
-p2 <- df2boxplot(file="model_auc.csv", ylab="AUC", methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("ind"), K = c(20))
-
-# RMSE
-p3 <- df2boxplot(file="model_rmse.csv", ylab=expression(paste("RMSE (", hat(beta), ")")), methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("ind"), K = c(20))
-
-# TPR
-p4 <- df2boxplot(file="model_tpr.csv", ylab="TPR", methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("ind"), K = c(20))
-
-# Precision
-p5 <- df2boxplot(file="model_ppv.csv", ylab="Precision", methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("ind"), K = c(20))
-
-ggarrange(p1, p2, p3, p4, p5, common.legend = TRUE, legend = "bottom", ncol = 2, nrow = 3)
-
-#--------------------
-## ggmixBIC vs pglmm vs cv_glmnet
-#--------------------
-
-# Model size
-q1 <- df2boxplot(file="model_size.csv", ylab=expression(paste("Model size (log"[10], " scale)", sep="")), methods = c("pglmmAIC", "pglmmBIC", "ggmixBIC", "cv_glmnetPC", "cv_glmnet"), geo = c("ind"), K = c(20), change_labels = FALSE)
-
-# AUC
-q2 <- df2boxplot(file="model_auc.csv", ylab="AUC", methods = c("pglmmAIC", "pglmmBIC", "ggmixBIC", "cv_glmnetPC", "cv_glmnet"), geo = c("ind"), K = c(20), change_labels = FALSE)
-
-# RMSE
-q3 <- df2boxplot(file="model_rmse.csv", ylab=expression(paste("RMSE (", hat(beta), ")")), methods = c("pglmmAIC", "pglmmBIC", "ggmixBIC", "cv_glmnetPC", "cv_glmnet"), geo = c("ind"), K = c(20), change_labels = FALSE)
-
-# TPR
-q4 <- df2boxplot(file="model_tpr.csv", ylab="TPR", methods = c("pglmmAIC", "pglmmBIC", "ggmixBIC", "cv_glmnetPC", "cv_glmnet"), geo = c("ind"), K = c(20), change_labels = FALSE)
-
-# Precision
-q5 <- df2boxplot(file="model_ppv.csv", ylab="Precision", methods = c("pglmmAIC", "pglmmBIC", "ggmixBIC", "cv_glmnetPC", "cv_glmnet"), geo = c("ind"), K = c(20), change_labels = FALSE)
-
-ggarrange(q1, q2, q3, q4, q5, common.legend = TRUE, legend = "bottom", ncol = 2, nrow = 3)
 
 #----------------------------
 # Related subjects from UKBB
 #----------------------------
 
-#--------------------
-## pglmm AIC vs BIC vs CV
-#--------------------
 kin = "ALL"
 
 # Model size
-p1_ <- df2boxplot(file="model_size.csv", ylab=expression(paste("Model size (log"[10], " scale)", sep="")), methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("UKBB"), kin_ = kin)
+q1_ <- df2boxplot(file="model_size.csv", ylab=expression(paste("Model size (log"[10], " scale)", sep="")), methods = c("pglmm", "pglmmAIC", "glmnet","glmnetPC", "ggmix"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE)
 
 # AUC
-p2_ <- df2boxplot(file="model_auc.csv", ylab="AUC", methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("UKBB"), kin_ = kin)
+q2_ <- df2boxplot(file="model_auc.csv", ylab="AUC", methods = c("pglmm", "pglmmAIC", "glmnet", "glmnetPC", "ggmix", "pglmmBIC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE)
 
 # RMSE
-p3_ <- df2boxplot(file="model_rmse.csv", ylab=expression(paste("RMSE (", hat(beta), ")")), methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("UKBB"), kin_ = kin)
+q3_ <- df2boxplot(file="model_rmse.csv", ylab=expression(paste("RMSE (", hat(beta), ")")), methods = c("pglmm", "pglmmAIC", "glmnet", "glmnetPC", "ggmix"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, ymin = 0.025, ymax = 0.04)
 
 # TPR
-p4_ <- df2boxplot(file="model_tpr.csv", ylab="TPR", methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("UKBB"), kin_ = kin)
+q4_ <- df2boxplot(file="model_tpr.csv", ylab="TPR", methods = c("pglmm", "pglmmAIC", "glmnet", "glmnetPC","ggmix"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE)
 
 # Precision
-p5_ <- df2boxplot(file="model_ppv.csv", ylab="Precision", methods = c("pglmmAIC", "pglmmBIC", "pglmm"), geo = c("UKBB"), kin_ = kin)
+q5_ <- df2boxplot(file="model_ppv.csv", ylab="Precision", methods = c("pglmm", "pglmmAIC", "glmnet", "glmnetPC", "ggmix"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, ymin=0, ymax=0.4)
 
-ggarrange(p1_, p2_, p3_, p4_, p5_, common.legend = TRUE, legend = "bottom", ncol = 2, nrow = 3)
-
-#--------------------
-## ggmixBIC vs pglmm vs cv_glmnet
-#--------------------
-kin = "ALL"
-
-# Model size
-q1_ <- df2boxplot(file="model_size.csv", ylab=expression(paste("Model size (log"[10], " scale)", sep="")), methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE)
-
-# AUC
-q2_ <- df2boxplot(file="model_auc.csv", ylab="AUC", methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE)
-
-# RMSE
-q3_ <- df2boxplot(file="model_rmse.csv", ylab=expression(paste("RMSE (", hat(beta), ")")), methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE)
-
-# TPR
-q4_ <- df2boxplot(file="model_tpr.csv", ylab="TPR", methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE)
-
-# Precision
-q5_ <- df2boxplot(file="model_ppv.csv", ylab="Precision", methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE)
-
-ggarrange(q1_, q2_, q3_, q4_, q5_, common.legend = TRUE, legend = "bottom", ncol = 2, nrow = 3)
+ggarrange(q2_, q1_, q3_, q4_, q5_, common.legend = TRUE, legend = "bottom", ncol = 2, nrow = 3)
 
 # Tables 
 # Model size
-df2boxplot(file="model_size.csv", methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE, logscale = FALSE)
+df2boxplot(file="model_size.csv", methods = c("pglmm", "pglmmAIC", "glmnet", "glmnetPC","ggmix"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE, logscale = FALSE)
 
 # AUC
-df2boxplot(file="model_auc.csv", ylab="AUC", methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE)
+df2boxplot(file="model_auc.csv", ylab="AUC", methods = c("pglmm", "pglmmAIC", "glmnet", "glmnetPC","ggmix", "pglmmAIC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE)
 
 # RMSE
-df2boxplot(file="model_rmse.csv", ylab=expression(paste("RMSE (", hat(beta), ")")), methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE)
+df2boxplot(file="model_rmse.csv", ylab=expression(paste("RMSE (", hat(beta), ")")), methods = c("pglmm", "pglmmAIC", "glmnet", "glmnetPC","ggmix"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE)
 
 # TPR
-df2boxplot(file="model_tpr.csv", ylab="TPR", methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE)
+df2boxplot(file="model_tpr.csv", ylab="TPR", methods = c("pglmm", "pglmmAIC", "glmnet", "glmnetPC","ggmix"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE)
 
 # Precision
-df2boxplot(file="model_ppv.csv", ylab="Precision", methods = c("pglmm", "pglmmAIC", "pglmmBIC", "glmnetPC"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE)
+df2boxplot(file="model_ppv.csv", ylab="Precision", methods = c("pglmm", "pglmmAIC", "glmnet", "glmnetPC","ggmix"), geo = c("UKBB"), kin_ = kin, change_labels = TRUE, print_table = TRUE)
+
+#----------------------------
+# Supplementary simulations
+#----------------------------
+supp_df <- read.csv("supp.csv")
+df <- rbind(
+  cbind(mse=supp_df[, "pglmm_mse_median"], method="pglmm", K=1:100),
+  cbind(mse=supp_df[, "pglmm_supp_mse_median"], method="pglmm_hessian", K=1:100),
+  cbind(mse=supp_df[, "glmnet_mse_median"], method="glmnetPC", K=1:100)
+) %>% as.data.frame()
+
+ggplot(df, aes(y=as.numeric(mse), x=as.numeric(K), colour=method, linetype=method)) +
+  geom_line() +
+  labs(y="MSE", x="lambda index")+
+  scale_colour_grey(start=0.7, end=0.1)+
+  theme_bw() +
+  theme(plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank() )+
+  #theme(panel.border= element_blank())+
+  theme(axis.line.x = element_line(color="black", size = 0.5),
+        axis.line.y = element_line(color="black", size = 0.5))
