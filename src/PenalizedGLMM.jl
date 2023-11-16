351154130fd88dc74e6c45e01b672db34d2dc66b
Julien St-Pierre <julien.st-pierre@mail.mcgill.com>
2023-11-15 21:52:33 -0500

    Added script for penalized GLM

--- src/PenalizedGLMM.jl
+++ src/PenalizedGLMM.jl
@@ -8,11 +8,13 @@ using SnpArrays
 import Base.show
 export pglmm_null
 export pglmm_cv
-export pglmm
+# export pglmm
+export pglm
 
 include("pglmm_null.jl")
-include("pglmm.jl")
+# include("pglmm.jl")
 include("pglmm_cv.jl")
 include("utils.jl")
+include("pglm.jl")
 
 end
